e84cf0f0e14853386c7110b23b590745a85a55aa
ashdwive <ashutosh.dwivedi@publicissapient.com>
2021-05-18 18:39:52 +0530

    Added AWS-Hosted API Path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseURL: 'http://ec2-13-233-237-18.ap-south-1.compute.amazonaws.com:5000/api/'
 };
 
 /*
